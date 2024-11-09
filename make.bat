f1ae0126ad59ffc92c2832feea680ba0373e3138
Тимур Комолов <komolov.timurka@mail.ru>
2024-11-09 16:50:44 +0300

    Change dependencies for supporting Windows 7

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -I./proto --python_out=./ratdream/proto_generated --grpc_python_out=./ratdream/proto_generated ./proto/cardio.proto
+python -m grpc_tools.protoc -I./proto --python_out=. --grpc_python_out=. ./proto/cardio.proto
