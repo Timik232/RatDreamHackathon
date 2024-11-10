3772d527f24071a713e97765ef8819a27b9bd69f
Тимур Комолов <komolov.timurka@mail.ru>
2024-11-10 10:04:00 +0300

    Delete unused files

--- ratdream/__init__.py
+++
@@ -1,7 +0,0 @@
-from .proto_generated import (
-    HelloReply,
-    HelloService,
-    add_HelloServiceServicer_to_server,
-)
-
-__all__ = ["HelloService", "add_HelloServiceServicer_to_server"]
--- ratdream/proto_generated/__init__.py
+++
@@ -1,4 +0,0 @@
-from .hello_pb2 import HelloReply
-from .hello_pb2_grpc import HelloService, add_HelloServiceServicer_to_server
-
-__all__ = ["HelloService", "add_HelloServiceServicer_to_server"]
